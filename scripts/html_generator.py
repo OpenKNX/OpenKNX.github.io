26f4d5727bcf7a633cfd227d6ec746cb7cfe8fb2
Cornelius Köpp <cornelius.koepp@fhdw.de>
2025-03-28 23:02:00 +0100

    WIP: Q&D Normalization of Device Names

--- scripts/html_generator.py
+++ scripts/html_generator.py
@@ -60,6 +60,65 @@ class HTMLGenerator:
         # TODO move, this should not be part of html rendering
         return "OpenKNX" in device_name
 
+    # TODO move, this should not be part of html rendering
+    device_name_map = {
+        "1TE-RP2040-SmartMF": "SmartMF-1TE-RP2040",
+        # "1TE-RP2040-SmartMF": "SmartMF-1TE-RP2040",
+        "AB-SmartHouse-BinaryClock": "AB-SmartHouse-BinaryClock",
+        "ABSmartHouse-BinaryInput": "AB-SmartHouse-BinaryInput",
+        "AB-SmartHouse-PresenceMR16": "AB-SmartHouse-PresenceMR16",
+        "AB-SmartHouse-PresenceMultiSensor": "AB-SmartHouse-PresenceMultiSensor",
+        "AB-SmartHouse-PresenceWall": "AB-SmartHouse-PresenceWall",
+        "AB-SmartHouse-SwitchActuator-REG6-8CH": "AB-SmartHouse-SwitchActuator-REG6-8CH",
+        "AccessControl": "AB-AccessControl",
+        "DeveloperBoard-JustForTesters": "?-DeveloperBoard-JustForTesters",
+        "firmware_UP1-GW-RS485": "OpenKNX-UP1-GW-RS485",
+        "GardenControl": "SmartMF-GardenControl",
+        "GW-UP1-IR": "?-GW-UP1-IR",
+        "IP-Router-REG1-Eth": "OpenKNX-IP-Router-REG1-Eth",
+        "IP-Router-REG1-LAN-TP-Base": "OpenKNX-IP-Router-REG1-LAN-TP-Base",
+        "OpenKNX-PiPico-BCU-Connector": "OpenKNX-PiPico-BCU-Connector",
+        "OpenKNX-REG1-Base-V0": "OpenKNX-REG1-Base-V0",
+        # "OpenKNX-REG1-BASE-V0": "OpenKNX-REG1-Base-V0",
+        # "OpenKNX-REG1-BASE-V0": "OpenKNX-REG1-Base-V0",
+        "OpenKNX-REG1-Base-V1": "OpenKNX-REG1-Base-V1",
+        # "OpenKNX-REG1-BASE-V1": "OpenKNX-REG1-Base-V1",
+        # "OpenKNX-REG1-BASE-V1": "OpenKNX-REG1-Base-V1",
+        "OpenKNX-REG1-Basismodul": "OpenKNX-REG1-Base-V1",
+        "OpenKNX-REG1-Basismodul-V0": "OpenKNX-REG1-Base-V0",
+        "OpenKNX-REG1-MultiSensor": "OpenKNX-REG1-MultiSensor",
+        "OpenKNX-UP1-8xSensor": "OpenKNX-UP1-8xSensor",
+        "PiPico_BCU_Connector": "OpenKNX-PiPico-BCU-Connector",
+        # "PiPico-BCU-Connector": "OpenKNX-PiPico-BCU-Connector",
+        # "PiPico-BCU-Connector": "OpenKNX-PiPico-BCU-Connector",
+        # "PiPico-BCU-Connector": "OpenKNX-PiPico-BCU-Connector",
+        "RealPresence": "?-RealPresence",
+        # "RealPresence": "?-RealPresence",
+        "RealPresence_v2.0": "?-RealPresence_v2.0",
+        "REG1_BASE_V0": "OpenKNX-REG1-Base-V0",
+        "REG1_BASE_V1": "OpenKNX-REG1-Base-V1",
+        "REG2_PIPICO_V1": "OpenKNX-PiPico-BCU-Connector",
+        "Sensormodul-v3.0-SAMD": "SmartMF-Sensormodul-v3.0-SAMD",
+        "Sensormodul-v3.1-SAMD": "SmartMF-Sensormodul-v3.1-SAMD",
+        "Sensormodul-v4.x-RP2040": "SmartMF-Sensormodul-v4.x-RP2040",
+        "Sensormodul-v4x-RP2040": "SmartMF-Sensormodul-v4.x-RP2040",
+        "SEN-UP1-8XTH": "OpenKNX-SEN-UP1-8XTH",
+        "SmartMF-1TE-RP2040": "SmartMF-1TE-RP2040",
+        "Smart-MF-eHZ-Schnittstelle": "Smart-MF-eHZ-Schnittstelle",
+        "Smart-MF-S0-Zaehlermodul": "Smart-MF-S0-Zaehlermodul",
+        "SmartMF-Sensormodul-RP2040": "SmartMF-Sensormodul-v4.x-RP2040",
+        "UP1-PM-HF": "OpenKNX-UP1-PM-HF",
+        "XIAO_MINI_V1": "OpenKNX-XIAO_MINI_V1",
+    }
+
+    def _hw_name_mapping(self, oam, hw_text):
+        # TODO move, this should not be part of html rendering
+        if hw_text in self.device_name_map:
+            return self.device_name_map[hw_text]
+        else:
+            logging.warning(f"Unknown Device Name in '{oam}': {hw_text}")
+            return f"(???)-{hw_text}"
+
     def generate_html_table(self, oam_dependencies, oam_hardware):
 
         # module -> usage_count
@@ -70,9 +129,10 @@ class HTMLGenerator:
                 modules_usage_count[key] += 1
         hardware_usage_count = defaultdict(int)
         hardware_other_usage_count = defaultdict(int)
-        for hw_list in oam_hardware.values():
-            logging.info(f"Devices {hw_list}")
-            for hw in hw_list:
+        for oam, hw_list in oam_hardware.items():
+            logging.info(f"Devices for {oam}: {hw_list}")
+            for hwText in hw_list:
+                hw = self._hw_name_mapping(oam, hwText)
                 logging.info(f"-> Device {hw}")
                 if self._is_open_device(hw):
                     hardware_usage_count[hw] += 1

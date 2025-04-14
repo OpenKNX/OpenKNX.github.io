d68141ceea9e4c51090bc99e2bce21fcdd774187
Cornelius Köpp <cornelius.koepp@fhdw.de>
2025-04-14 20:54:09 +0200

    App-XML-Statistics: Use Dynamic-Element in ApplicationProgram

--- scripts/app_sizing_stat.py
+++ scripts/app_sizing_stat.py
@@ -106,15 +106,15 @@ class AppSizingStat:
                 self.choose_element_count = len(dynamic.findall(".//{*}Choose"))
                 self.assign_element_count = len(dynamic.findall(".//{*}Assign"))
             
-            # Count ParameterBlock with Inline!="true"
-            parameter_blocks = root.findall(".//{*}Dynamic/{*}ParameterBlock[@Inline!='true']")
-            self.parameter_block_count = len(parameter_blocks)
+                # Count ParameterBlock with Inline!="true"
+                parameter_blocks = dynamic.findall(".//{*}ParameterBlock[@Inline!='true']")
+                self.parameter_block_count = len(parameter_blocks)
 
-            # Determine the maximum number of ParamRefRef elements within such ParameterBlock
-            self.max_param_ref_ref_count = 0
-            for block in parameter_blocks:
-                param_ref_refs = block.findall(".//{*}ParamRefRef")
-                self.max_param_ref_ref_count = max(self.max_param_ref_ref_count, len(param_ref_refs))
+                # Determine the maximum number of ParamRefRef elements within such ParameterBlock
+                self.max_param_ref_ref_count = 0
+                for block in parameter_blocks:
+                    param_ref_refs = block.findall(".//{*}ParamRefRef")
+                    self.max_param_ref_ref_count = max(self.max_param_ref_ref_count, len(param_ref_refs))
                 
         except Exception as e:
             print(f"Error processing XML: {e}")
